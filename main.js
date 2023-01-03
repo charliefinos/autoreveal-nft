3b2aa69327c5dfc1199e254e07210a5c9e19f105
charlyfinos <charlyfinos@gmail.com>
2023-01-03 19:56:43 -0300

    refactor

--- main.js
+++ main.js
@@ -1,38 +1,37 @@
 const fs = require("fs");
 const uploadToPinataAndSaveHashes = require("./uploader.js");
 const addNft = require("./addNft");
-// Upload 10 nfts with their own metadata and store them on mongodb
 
+// Read current nft name, description and attributes
+async function readMetadata(nftId) {
+  const file = fs.readFileSync(`./assets/metadata/${nftId}.json`);
+  return JSON.parse(file);
+}
+
+// return uploaded ipfs hash
+async function uploadAsset(path, nftId) {
+  const uploadedFile = await uploadToPinataAndSaveHashes(path, nftId);
+  return uploadedFile.IpfsHash;
+}
+
+// Upload 10 nfts with their own metadata and store them on mongodb
 async function main() {
   for (let i = 0; i < 10; i++) {
-    // read all assets, upload and store right metadata
-    const highResImage = await uploadToPinataAndSaveHashes(
-      `./assets/high_res_images/${i}.png`,
-      i
-    );
-    const track = await uploadToPinataAndSaveHashes(
-      `./assets/tracks/${i}.wav`,
-      i
-    );
-    const image = await uploadToPinataAndSaveHashes(
-      `./assets/images/${i}.png`,
-      i
-    );
-
-    // Read current nft name, description and attributes
-
-    const file = fs.readFileSync(`./assets/metadata/${i}.json`);
-
-    const readable = JSON.parse(file);
+    const [highResImage, track, image, metadata] = await Promise.all([
+      uploadAsset(`./assets/high_res_images/${i}.png`, i),
+      uploadAsset(`./assets/tracks/${i}.wav`, i),
+      uploadAsset(`./assets/images/${i}.png`, i),
+      readMetadata(i),
+    ]);
 
     const res = await addNft(
-      image.IpfsHash,
-      highResImage.IpfsHash,
-      track.IpfsHash,
+      image,
+      highResImage,
+      track,
       i,
-      readable.name,
-      readable.description,
-      readable.attributes
+      metadata.name,
+      metadata.description,
+      metadata.attributes
     );
     console.log(res);
     console.log(`NFT ID:${i}`);
